f2928ac675167d79d49842eaf3d68afb9abbbf30
soniapigot <sonia.pigot@live.fr>
2015-11-03 14:43:31 +0100

    int en string

--- src/main/java/net/apispark/webapi/representation/Company.java
+++ src/main/java/net/apispark/webapi/representation/Company.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class Company implements Serializable {
 
-    private int id;
+    private String id;
     private String name;
     private List<String> tags;
     private Address address;
